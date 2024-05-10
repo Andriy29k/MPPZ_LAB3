cbaea2318cf4965c56b52eefdf0854797e514edb
Andriy29k <adnriykorotchuk345@gmail.com>
2024-05-10 11:00:01 +0300

    rework architecture

---
+++ mppz_refactoring-main/src/main/java/com/rshu/mppz/dataLink/Customers.java
@@ -0,0 +1,22 @@
+package com.rshu.mppz.dataLink;
+
+//import javax.xml.bind.annotation.XmlElement;
+//import javax.xml.bind.annotation.XmlRootElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@XmlRootElement
+public class Customers {
+
+	private List<Customer> customers;
+
+//	@XmlElement
+	public List<Customer> getCustomerList() {
+		if (customers == null) {
+			customers = new ArrayList<>();
+		}
+		return customers;
+	}
+
+}
---
+++ mppz_refactoring-main/src/main/java/com/rshu/mppz/domain/CustomerDetails.java
@@ -0,0 +1,5 @@
+package com.rshu.mppz.domain;
+
+public class CustomerDetails {
+
+}
